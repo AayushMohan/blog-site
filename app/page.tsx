8aa95fbc8bbc56d725d65fd3be407e73aaf990ec
AayushMohan <aayushmohan1702@gmail.com>
2023-01-30 22:32:08 +0530

    Added Tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const page = () => {
   return (
     <div>
-      <h1>Page</h1>
+      <h1 className="text-4xl">Welcome to the Blog</h1>
       <p>Some Content</p>
     </div>
   );
