5c61593edf1b7053dfd4780a920c72f4314feda0
AayushMohan <aayushmohan1702@gmail.com>
2023-02-05 22:24:34 +0530

    Added Header In Layout

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
     <html>
       <body>
         {/* Header */}
-        {/* <Header /> */}
+        <Header />
         {/* Banner */}
         <Banner />
         {children}
