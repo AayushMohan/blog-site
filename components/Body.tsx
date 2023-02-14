5ddaa8c1ddaa081ca32790394bfa81eecb32625c
AayushMohan <aayushmohan1702@gmail.com>
2023-02-14 10:00:00 +0530

    Added Image in Body

--- components/Body.tsx
+++ components/Body.tsx
@@ -1,9 +1,11 @@
+import Image from "next/image";
 import React from "react";
 
 const Body = () => {
   return (
     <div>
       <h1>Main Container</h1>
+      <Image src="" width={20} height={20} alt="" />
     </div>
   );
 };
