5b102df862518bbe282a6dc51c3dd9bae688977a
AayushMohan <aayushmohan1702@gmail.com>
2023-02-14 10:00:00 +0530

    Added description

--- components/Body.tsx
+++ components/Body.tsx
@@ -7,6 +7,13 @@ const Body = () => {
       <h1>Main Container</h1>
       {/* <Image src="" width={20} height={20} alt="The Main Image for the Blog" /> */}
       <h1>The Apple VR headset is Finally here!</h1>
+      <p>
+        Apple has been working on a VR headset for a long time now. The Lorem
+        ipsum dolor sit amet, consectetur adipisicing elit. Dolorem vero
+        obcaecati voluptatem quas eos suscipit at ex iste voluptatum officiis
+        quis sit fugit unde, dicta eligendi facilis esse officia amet!
+        Repellendus, voluptatum. Lorem ipsum dolor sit amet consectetur
+      </p>
     </div>
   );
 };
