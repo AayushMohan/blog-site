72de13acc211271827e578fa4bdb33c350d708fd
AayushMohan <aayushmohan1702@gmail.com>
2023-02-15 10:00:00 +0530

    Added exit-preview

--- pages/api/exit-preview.ts
+++ pages/api/exit-preview.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-export default function preview(
+export default function exit(
  req: NextApiRequest,
  res: NextApiResponse
 ) {
