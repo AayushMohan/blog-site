b1b7a0f74b5a682b4a2100b24f8570e3be91eb7d
AayushMohan <aayushmohan1702@gmail.com>
2023-02-17 10:00:00 +0530

    Added Posts Fetching Query

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,17 +1,20 @@
 import { previewData } from "next/headers";
 import { groq } from "next-sanity";
+import { client } from "../../lib/sanity.client";
 
 const query = groq`*[_type == "post"{
   ...,
   author->,
   categories[]->,
-} | order(_createdAt desc))`;
+} | order(_createdAt desc)`;
 
-const page = () => {
+const page = async () => {
   if (previewData()) {
     return <div>Preview Mode</div>;
   }
 
+  const posts = await client.fetch(query);
+  console.log(posts);
   return (
     <div>
       <h1>Not in Preview Mode</h1>
